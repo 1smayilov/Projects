98eacdc9c2c1b7bd445c7715dcf522484ab0b83b
Elvin Ismayilov <0utsawg@Gmail.com>
2024-06-24 17:58:00 +0400

    Project 24.06.2024

---
+++ Project/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // Generic constraint - generic kisit
+    // class - referans tip ola biler demekdir, ancaq IEntity in daxilinde olanlar
+    //new() - new lene bilir olmalidir, IEntity olmasin deye bunu edirik ancaq daxilindekiler olsun
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        // Xüsusi məsələn categoriyası 2 olanları gətir, və yaxud adları bu olanları gətir
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Project/DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context : Db tablolari ile proyekt classlarini baglamaq    
+    public class NortwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-ADHI170;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+
+}
